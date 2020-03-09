5e867e0b7389cbd746347ce7e551f646b02be23f
artsteffen13 <artsteffen13@hotmail.com>
2020-03-09 12:55:03 -0400

    bug fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: "There"})
+    res.send('Hello')
 });
 
 const PORT = process.env.port || 5000;
